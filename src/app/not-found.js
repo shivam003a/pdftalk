c593cc848888d755f976fb455d9a508d3cf544e7
Shivam Singh <shivam003a@gmail.com>
2025-07-05 17:32:10 +0530

    minor cosmetic change

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -9,10 +9,10 @@ export default function NotFound() {
                 <Navbar />
 
                 {/* not found content */}
-                <div className="min-h-[calc(100vh-72px)] flex flex-col items-center justify-center bg-primary text-s-text">
+                <div className="min-h-[calc(100vh-72px)] flex flex-col items-center justify-center bg-primary text-s-text p-4">
                     <div className="w-full relative flex flex-col items-center justify-center">
                         <h1 className="text-[128px] font-bold text-secondary tracking-widest font-poppins">404</h1>
-                        <p className="text-4xl font-semibold font-poppins tracking-wider absolute top-1/2 -translate-y-1/2">Lost? This page isn’t here</p>
+                        <p className="text-4xl font-semibold font-poppins tracking-wider absolute top-1/2 -translate-y-1/2 text-center">Lost? This page isn’t here</p>
                     </div>
                     <Link
                         href={"/"}
