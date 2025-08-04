c03f93cb10819557bcd5b8f2957bce70fbd4c9a6
Shivam Singh <shivam003a@gmail.com>
2025-08-04 19:46:30 +0530

    minor fe change

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,18 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
         <Toaster position='top-right' />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
